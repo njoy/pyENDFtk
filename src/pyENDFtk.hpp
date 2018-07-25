61149196a9e7af4e02dad90436a905ad52733aaa
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-07-25 16:32:44 -0600

    It works

--- src/pyENDFtk.hpp
+++ src/pyENDFtk.hpp
@@ -1,8 +1,13 @@
 #ifndef PYENDFTK_HPP
 #define PYENDFTK_HPP
 
+#include "pybind11/pybind11.h"
+
+namespace py = pybind11;
+
 namespace njoy {
 namespace pyENDFtk {
+#include "pyENDFtk/test.hpp"
 } // namespace pyENDFtk
 } // namespace njoy
 #endif // PYENDFTK_HPP
---
+++ src/pyENDFtk/test.hpp
@@ -0,0 +1,32 @@
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(pyENDFtk, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: cmake_example
+        .. autosummary::
+           :toctree: _generate
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
+
