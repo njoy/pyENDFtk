828e04c71c5c939008447268503e42172069804d
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-07-26 07:37:40 -0600

    End of learning time.

--- src/pyENDFtk.hpp
+++ src/pyENDFtk.hpp
@@ -5,9 +5,27 @@
 
 namespace py = pybind11;
 
-namespace njoy {
-namespace pyENDFtk {
 #include "pyENDFtk/test.hpp"
-} // namespace pyENDFtk
-} // namespace njoy
-#endif // PYENDFTK_HPP
+
+PYBIND11_MODULE(ENDFtk, m) {
+
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: cmake_example
+        .. autosummary::
+           :toctree: _generate
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+}
